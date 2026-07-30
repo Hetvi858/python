abd88cbb0f0fc0277ff285a7a8a9b46b511ea469
Hetvi chotara <hetvichotara@gmail.com>
2026-07-30 12:53:00 +0530

    Add files via upload

---
+++ second.py
@@ -0,0 +1,20 @@
+i=10
+flt=10.5
+text="Hiii"
+bool1=True
+
+print("int :",i,type(i))
+print("float :",flt,type(flt)) 
+print("text :",text,type(text))
+print("bool :",bool1,type(bool1))
+print()
+print("Type casting")
+
+a=20
+b=float(a)
+c=str(b)
+d=bool(c)
+
+print("Integer to float :",b,type(b))
+print("Float to String :",c,type(c))
+print("String to Bool :",d,type(d))
