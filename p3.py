c913d3ca91847cb82ac010883b64029b43307618
Hetvi chotara <hetvichotara@gmail.com>
2026-07-30 13:05:10 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,18 @@
+a = int(input("enter first num: "))
+b = int(input("enter second num: "))
+
+# arithmetic 
+print("addition =", a + b)
+print("subtraction =", a - b)
+print("multiplication =", a * b)
+print("division =", a / b)
+
+# relational
+print("a > b =", a > b)
+print("a < b =", a < b)
+print("a != b =", a != b)
+
+# logical
+# Using user-inputted 'b' for the logical checks
+print("b < 5 or b == 7:", b < 5 or b == 7)
+print("b < 5 or b > 10:", b < 5 or b > 10)
