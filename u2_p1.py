98b1466be6bf876ed954ee7f4a29744ba0ef7921
Hetvi chotara <hetvichotara@gmail.com>
2026-08-11 13:02:03 +0530

    Add files via upload

---
+++ u2_p1.py
@@ -0,0 +1,25 @@
+#Write a program to demonstrate conditional statements using if if-else and if-elif-else.
+
+marks = ('91','75','60','50','33')
+print('enter your marks:')
+
+
+if  marks>=90:
+    print('Grade:A')
+    
+elif marks>=75:
+     print('Grade:B')
+
+elif marks>=60:
+     print('Grade:C')
+
+elif marks>=50:
+     print('Grade:D')
+     
+elif marks>=33:
+    print('Grade:E')
+    
+else:
+    print('fail')
+    
+
