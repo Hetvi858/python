1a471e13386fcaaf2d0cff3b980be96f506f3e90
Hetvi chotara <hetvichotara@gmail.com>
2026-08-11 13:03:08 +0530

    Add files via upload

---
+++ u2_p2.py
@@ -0,0 +1,16 @@
+#Write a program to check whether a number is positive negative or zero using nested conditions.
+
+num=float(input('enter a number:'))
+
+if num>=0:
+    
+    if num==0:
+        
+        print('number is zero')
+    else:
+        print('number is positive')
+else:
+        print('number is negetive')
+
+
+
