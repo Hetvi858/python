5f71b6cf48810eb9b34de10fd1c9e0946e0ca594
Hetvi chotara <hetvichotara@gmail.com>
2026-08-11 12:43:47 +0530

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,23 @@
+#Write a program to illustrate the use of tuples and sets with basic operations. 
+
+#tuple:
+
+colors= ('red','blue','white')
+print(colors)
+
+#indexing in tuple:
+
+print('first item:',colors[0])
+print('second item:',colors[-1])
+print('slicing [0:2]:',colors[0:2])
+
+#set:
+
+name= ['shital','nisha','hetvi']
+print(name)
+
+#indexing in set:
+
+print('first name:',name[0])
+print('second name:',name[-1])
+print('last name:',name[0:2])
