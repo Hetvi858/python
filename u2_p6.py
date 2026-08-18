70db8a62509169301a7f9317dc4f83fbade6d277
Hetvi chotara <hetvichotara@gmail.com>
2026-08-18 12:08:21 +0530

    Add files via upload

---
+++ u2_p6.py
@@ -0,0 +1,23 @@
+#Write a program to iterate over lists strings and dictionaries using loops.
+
+#List
+numbers= [10,20,30,40,50]
+print('List elements:')
+for num in numbers:
+    print(num)
+    
+
+#string
+text='python'
+print('string chartecters:')
+for ch in text:
+    print(ch)
+    
+
+#Dictionaries
+division = {
+    'A' 'B' 'C'
+    }
+print('dictionaries elements:')
+for e in division:
+    print(division)
