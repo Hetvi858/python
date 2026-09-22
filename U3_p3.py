59eb410656b9f40768af7216de4f4f891b4b566f
Hetvi chotara <hetvichotara@gmail.com>
2026-09-22 12:42:43 +0530

    Add files via upload

---
+++ U3_p3.py
@@ -0,0 +1,13 @@
+#Write a program to perform mathematical operations using math module. 
+
+
+import math
+
+num = float(input("Enter a number: "))
+
+print("Square root:", math.sqrt(num))
+print("Power (square):", math.pow(num, 2))
+print("Ceiling value:", math.ceil(num))
+print("Floor value:", math.floor(num))
+print("Factorial:", math.factorial(int(num)))
+print("Absolute value:", math.fabs(num))
